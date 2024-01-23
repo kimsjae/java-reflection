e80b673762ffb68200ec6081d2e6c13aa82ba611
kimseongjae <stwin755@gmail.com>
2024-01-23 11:43:07 +0900

    자바리플렉션

---
+++ src/main/java/ex01/UserController.java
@@ -0,0 +1,12 @@
+package ex01;
+
+public class UserController {
+
+    public void login() {
+        System.out.println("로그인 호출됨");
+    }
+
+    public void join() {
+        System.out.println("회원가입 호출됨");
+    }
+}
